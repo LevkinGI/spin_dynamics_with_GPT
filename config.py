5c35c8c9380966783fe7a144a4c1daab801bc2f1
Grigory <44641914+LevkinGI@users.noreply.github.com>
2025-12-25 23:12:08 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class SimParams:
-    alpha_scale: float   # множитель для коэффициента затухания α
-    chi_scale:   float   # множитель для магнитной восприимчивости χ (теперь скаляр)
-    k_scale:  float  # множитель для K(T)
-    m_scale: float  # множитель массива m
